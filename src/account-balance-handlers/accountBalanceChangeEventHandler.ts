4df04e0d7fc9b9a3334ef86ae4b954f609db1f0e
Paul <zhpv96@gmail.com>
2024-12-05 14:19:19 +0300

    add UserAccountBalance

--- src/account-balance-handlers/accountBalanceChangeEventHandler.ts
+++ src/account-balance-handlers/accountBalanceChangeEventHandler.ts
@@ -1,5 +1,5 @@
-import { AccountBalance, AccountBalanceChangeEvent } from "generated";
-import { getISOTime } from "../utils";
+import { AccountBalance, AccountBalanceChangeEvent, UserAccountBalance } from "generated";
+import { decodeI64, getISOTime } from "../utils";
 import { getHash } from "../utils";
 import { nanoid } from "nanoid";
 
@@ -17,13 +17,21 @@ AccountBalance.AccountBalanceChangeEvent.handlerWithLoader({
 		const accountBalanceChangeEvent: AccountBalanceChangeEvent = {
 			id: nanoid(),
 			trader: event.params.trader.payload.bits,
+			market: event.srcAddress,
 			baseToken: event.params.base_token.bits,
-			takerPositionSize: event.params.account_balance.taker_position_size.underlying,
-			takerOpenNotional: event.params.account_balance.taker_open_notional.underlying,
-			lastTwPremiumGrowthGlobal: event.params.account_balance.last_tw_premium_growth_global.underlying,
-			timestamp: event.params.timestamp,
+			takerPositionSize: decodeI64(event.params.account_balance.taker_position_size.underlying),
+			takerOpenNotional: decodeI64(event.params.account_balance.taker_open_notional.underlying),
+			lastTwPremiumGrowthGlobal: decodeI64(event.params.account_balance.last_tw_premium_growth_global.underlying),
+			timestamp: getISOTime(event.block.time),
 			txId: event.transaction.id
 		};
 		context.AccountBalanceChangeEvent.set(accountBalanceChangeEvent);
+
+		const accountBalance: UserAccountBalance = {
+			...accountBalanceChangeEvent,
+			id: getHash(`${event.params.trader.payload.bits}-${event.srcAddress}`),
+			timestamp: getISOTime(event.block.time)
+		};
+		context.UserAccountBalance.set(accountBalance);
 	},
 });
